f312d0bafe52710464d98b397357584c9581bb61
nyamero0 <geca.matic.up@phinmaed.com>
2024-04-23 11:27:14 +0800

    latest

--- js/hotel/script.js
+++ js/hotel/script.js
@@ -1,7 +1,16 @@
 const sliderUl = Array.from(document.querySelector("#slider > ul").children);
+sliderUl.map(e => {
+    e.style.display = "none";
+    e.style.opacity = "0";
+});
 setInterval(() => {
     const curr = sliderUl.shift();
-    curr.style.order = "";
+    curr.style.order = "2";
+    curr.style.display = "none";
+    curr.style.opacity = "0";
     sliderUl.push(curr);
     sliderUl[0].style.order = "1";
-}, 8000);
+    sliderUl[0].style.display = "initial";
+    sliderUl[0].style.opacity = "1.0";
+
+}, 3000);
